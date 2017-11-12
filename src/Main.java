a66f2564ca238c02442feea51882982f12106188
Bladimir Baez <bladimirbaezt@gmail.com>
2017-11-12 01:23:14 -0500

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Bladimir Baez
+ * @version 1.0
+ * @since 11/12/17
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print("Testing");
+    }
+}
